9f5e07f14de2fc411477d6aae60f40bd827a95eb
Lewis Lakerink <llakerink@swin.edu.au>
2020-12-04 13:05:26 +1100

    Add an empty environment file that will be sourced by the job controller server
    Summary: @bypass-review because this is a trivial change
    
    Test Plan: N/A
    
    Reviewers: mr-asher
    
    Differential Revision: https://phab.adacs.org.au/D367

---
+++ env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Place any environment specific configuration here. This file will be sourced before the virtualenv is activated. For example, you may load a python module here that is then used by the virtualenv
+
+
